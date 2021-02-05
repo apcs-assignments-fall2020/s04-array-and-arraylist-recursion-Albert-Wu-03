c0d055dcb8503584551d7a6f5e05ea6749986395
Albert-Wu-03 <22qwu@athenian.org>
2021-02-05 03:32:39 +0000

    Rescursion array

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,30 +4,43 @@ public class MyMain {
 
     // Wrapper method
     public static boolean isSorted(ArrayList<Integer> list) {
-        // YOUR CODE HERE
-        return false;
+        return isSortedRec(list, 0);
     }
 
     // This recursive method checks if the array is sorted in 
     // non-decreasing order
     public static boolean isSortedRec(ArrayList<Integer> list, int i) {
-        // YOUR CODE HERE
-        return false;
+        if(i == list.size()-1){
+            return true;
+        }else if(list.get(i) > list.get(i+1)){
+            return false;
+        }else{
+            return isSortedRec(list, i+1);
+        }
     }
 
 
 
      // Wrapper method
     public static boolean hasCountCopies(int[] arr, int x, int count) {
-        // YOUR CODE HERE
-        return false;
+        return hasCountCopiesRec(arr, x, count, 0);
     }
 
     // This recursive method checks if the array contains exactly
     // count copies of the integer x
     public static boolean hasCountCopiesRec(int[] arr, int x, int count, int i) {
-        // YOUR CODE HERE
-        return false;
+        if(count < 0){
+            return false;
+        }else if(i == arr.length){
+            if(count == 0){
+                return true;
+            }
+            return false;
+        }else if(arr[i] == x){
+            return hasCountCopiesRec(arr, x, count-1, i+1);
+        }else{
+            return hasCountCopiesRec(arr, x, count, i+1);
+        }
     }
 
 
@@ -35,14 +48,20 @@ public class MyMain {
 
     // Wrapper method
     public static boolean binarySearch(int[] arr, int num) {
-        // YOUR CODE HERE
-        return false;
+        return binarySearchRec(arr, num, 0, arr.length-1);
     }
 
     // This recursive method calls binary search on the array
     public static boolean binarySearchRec(int[] arr, int num, int lowerBound, int upperBound) {        
-        // YOUR CODE HERE
-        return false;
+        if(upperBound == lowerBound){
+            return false;
+        }else if(arr[lowerBound + (upperBound - lowerBound) / 2] == num){
+            return true;
+        }else if(arr[lowerBound + (upperBound - lowerBound) / 2] < num){
+            return binarySearchRec(arr, num, (lowerBound + (upperBound - lowerBound) / 2) + 1, upperBound);
+        }else{
+            return binarySearchRec(arr, num, lowerBound, (lowerBound + (upperBound - lowerBound) / 2) -1);
+        } 
     }
 
     public static void main(String[] args) {
